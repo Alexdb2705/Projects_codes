eb312f67e7d21757f04a649854fd93d4026a029d
Alexdb2705 <alejandro.barrioa@uah.es>
2025-06-09 12:08:38 +0200

    Changes in old codes

--- Old_Versions/GenDat/main.py
+++ Old_Versions/GenDat/main.py
@@ -58,54 +58,12 @@ def main(args):
         for module in comandos.keys():                                          # for each module  
             subparsers = module.add_subparser(subparsers)                       # register each module command parser
         
-        options  = base_parser.parse_args(args)                                 # now parse cmdline args to full modules specs
-
     except Exception as exc:                                                    # if something bad happened 
         print(f"{exc}\n")                                                       # show argparse error
-        base_parser.print_help()                                                # help the user
-    else:                                                                       # else command parsing went Ok
-        comando(options.module, options, test = options.test)                   # run the module with appropieate options and test mode
+        base_parser.print_help()                                                # run the module with appropieate options and test mode
     finally:                                                                    # Main program exit.
         print(f"Programa terminado")
 
-def comando(module, args, test = False):
-    """
-        comando: Module -> Argparse.Namespace -> Bool -> IO ()
-        Commands module runner
-        Run the given module using the namespace parsed form command line switches
-    """
-    edition     = '#GenDat2024 V0.1 (CodeName: Johny Five)'
-    start       = timer()
-    try:
-        print(f"{description} {edition}\n")
-        if test:                                                                # if test suite solicited
-            test_module(args, None)                                             # run test suite aborad the module
-        else:                                                                   # else
-            module.main(args)                                                   # run the module main subroutine
-    except P.ParseError as PE:      print(f"Error found during parsing: {PE}")  # Error found parsing any file     
-    except KeyboardInterrupt:       print(f"Program abort by Ctrl-C:")          # if user aborts bail out nicely 
-    #except Exception as Exc:        print(f"Exception found: {Exc}")            # if an exception is caught bail out nicely 
-    #except:                         print(f"Unknown error!")                    # Opps!
-    finally:                                                                    # before finishing
-        stop    = timer()                                                       # record time
-        elapsed = timedelta(seconds = stop - start)                             # compute duration of the run         
-        msg     = module.command_description                                    # what's the bussines inside
-        print(f"\n{msg} terminó en {elapsed}\n\nHave a nice day!\n")              # tell da user
-    return None
-
-def test_module(args, _input):
-    """
-        test_module :: NameSpace -> None -> IO ()
-        Run any test suite aboard the given module
-    """
-    import doctest                                                              # import doctest module
-    (f, t) = doctest.testmod(                                                   # run test suite on the choosen module
-             m          = args.module                                           # scan this module
-            ,verbose    = False                                                 # supress chitchat 
-            ,report     = True                                                  # show report
-        )
-    print(f"\n{f} tests fallidos de {t} tests definidos en '{args.module.__name__}'.\n")
-    return None
 
 if __name__ == "__main__":                                                      # if this module gets exe'd
     main(sys.argv[1:])                                                          # call the main entry point
