9fc501bf5766f12ad89acf984914de38859f498c
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-06-09 11:57:20 +0200

    Update Old_Versions code

--- Old_Versions/GenDat/commands/dataset_parallel.py
+++ Old_Versions/GenDat/commands/dataset_parallel.py
@@ -27,17 +27,10 @@ def add_subparser(subparsers):
     """
         add_subparser:: ArgParse -> ArgParse
         Add asubparser for command line switches need for this very module to run properly
-        An additional "--test" command line switch allows to perform doctest tests from the main program for this module
     """
-    from library.file_checks    import check_file_arg
-    is_reachable = lambda s: check_file_arg(                                    # lambda for file checking
-                 argparse.ArgumentTypeError(f"'{s}' no encontrado o accesible!")# exception to raise
-                ,Path(s))                                                       # Path to check
-
     command_parser              = subparsers.add_parser(command_name, description = command_description)
     ### Additional command line arguments needed for this puzzle like data files etc..
-    command_parser.add_argument("--test", help = f"test suite del módulo {__name__}", action='store_true') # optional built-in test suite switch
-
+   
     if not '--test' in sys.argv:                                                # if not '--test' requested show additional params
         command_parser.add_argument('-a', '--background',  action=argparse.BooleanOptionalAction, default=False,
                                     help = "¿Se está ejecutando con el python de Blender?")
